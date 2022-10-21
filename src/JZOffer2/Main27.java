5ca95d790ef0e5d98ee99dbdcd81c2698673634a
linjiayu <531467600@qq.com>
2022-10-21 14:21:28 +0800

    树与顺序输出

---
+++ src/JZOffer2/Main27.java
@@ -0,0 +1,14 @@
+package JZOffer2;
+
+public class Main27 {
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode left = mirrorTree(root.left);
+        TreeNode right = mirrorTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
---
+++ src/JZOffer2/Main28.java
@@ -0,0 +1,17 @@
+package JZOffer2;
+
+public class Main28 {
+    public boolean isSymmetric(TreeNode root) {
+        return isSame(root, root);
+    }
+
+    public boolean isSame(TreeNode root, TreeNode mirror){
+        if(root == null && mirror == null){
+            return true;
+        }
+        if(root == null || mirror == null){
+            return false;
+        }
+        return root.val == mirror.val&&isSame(root.left, mirror.right) && isSame(root.right, mirror.left);
+    }
+}
