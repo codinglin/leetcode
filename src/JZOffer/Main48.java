938ca55c1f9916090151e2d594e78daa622f99ed
linjiayu <531467600@qq.com>
2022-10-25 20:55:14 +0800

    相信自己~

--- src/JZOffer/Main48.java
+++ src/JZOffer/Main48.java
@@ -1,5 +1,8 @@
 package JZOffer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main48 {
     public static int lengthOfLongestSubstring(String s) {
         if(s.length() == 0){
@@ -25,5 +28,56 @@ public class Main48 {
 
     public static void main(String[] args) {
         lengthOfLongestSubstring("aabaab!bb");
+    }
+}
+
+/**
+ * 动态规划
+ *  状态定义：dp[j]代表以字符s[j]为结尾的"最长不重复子字符串"的长度。
+ *  转移方程：固定右边界j，设字符s[j]左边距离最近的相同字符为s[i]，即s[i] = s[j]
+ *      1.当i<0,即s[j]左边无相同字符，则dp[j] = dp[j-1] + 1
+ *      2.当dp[j-1] < j-i，说明字符s[i]在子字符串dp[j-1]区间之外，则dp[j] = dp[j-1] + 1;
+ *      3.当dp[j-1] >= j-i,说明字符s[i]在子字符串dp[j-1]区间之中，则dp[j]的左边界由s[i]决定，即dp[j]=j-i;
+ */
+class Main48_1{
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int res = 0, tmp = 0;
+        for(int j = 0; j<s.length(); j++){
+            int i = dic.getOrDefault(s.charAt(j), -1);
+            dic.put(s.charAt(j), j);
+            tmp = tmp < j-i? tmp+1:j-i;
+            res = Math.max(res,tmp);
+        }
+        return res;
+    }
+
+    public int lengthOfLongestSubstring1(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int res = 0, tmp = 0;
+        for(int j = 0; j < s.length(); j++) {
+            int i = j - 1;
+            while(i >= 0 && s.charAt(i) != s.charAt(j)) i--; // 线性查找 i
+            tmp = tmp < j - i ? tmp + 1 : j - i; // dp[j - 1] -> dp[j]
+            res = Math.max(res, tmp); // max(dp[j - 1], dp[j])
+        }
+        return res;
+    }
+}
+
+/**
+ * 双指针
+ */
+class Main48_2{
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int i = -1, res = 0;
+        for(int j = 0; j < s.length(); j++) {
+            if(dic.containsKey(s.charAt(j)))
+                i = Math.max(i, dic.get(s.charAt(j))); // 更新左指针 i
+            dic.put(s.charAt(j), j); // 哈希表记录
+            res = Math.max(res, j - i); // 更新结果
+        }
+        return res;
     }
 }
---
+++ src/JZOffer/Main50.java
@@ -0,0 +1,59 @@
+package JZOffer;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * 使用哈希表存储频数
+ */
+public class Main50 {
+    public char firstUniqChar(String s) {
+        Map<Character, Integer> frequency = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            frequency.put(ch, frequency.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            if (frequency.get(s.charAt(i)) == 1) {
+                return s.charAt(i);
+            }
+        }
+        return ' ';
+    }
+}
+
+/**
+ * 队列
+ */
+class Main50_1{
+    public char firstUniqChar(String s) {
+        Map<Character, Integer> position = new HashMap<>();
+        Queue<Pair> queue = new LinkedList<Pair>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if(!position.containsKey(ch)){
+                position.put(ch, i);
+                queue.offer(new Pair(ch, i));
+            }else{
+                position.put(ch, -1);
+                while(!queue.isEmpty() && position.get(queue.peek().ch) == -1){
+                    queue.poll();
+                }
+            }
+        }
+        return queue.isEmpty() ? ' ':queue.poll().ch;
+    }
+
+    class Pair{
+        char ch;
+        int pos;
+
+        Pair(char ch, int pos){
+            this.ch = ch;
+            this.pos = pos;
+        }
+    }
+}
