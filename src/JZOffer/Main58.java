be1377a320017e536e5527041b330ff192ad824a
linjiayu <531467600@qq.com>
2022-10-29 16:55:44 +0800

    小小咸鱼

--- src/JZOffer/Main58.java
+++ src/JZOffer/Main58.java
@@ -1,4 +1,103 @@
 package JZOffer;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 自行编写对应函数
+ */
 public class Main58 {
+    public String reverseWords(String s){
+        StringBuilder sb = trimSpace(s);
+        // 翻转字符串
+        reverse(sb, 0, sb.length() - 1);
+
+        // 翻转每个单词
+        reverseEachWord(sb);
+
+        return sb.toString();
+    }
+
+    private StringBuilder trimSpace(String s){
+        int left = 0, right = s.length() - 1;
+        // 去掉字符串开头的空白字符
+        while (left <= right && s.charAt(left) == ' ') {
+            ++left;
+        }
+
+        // 去掉字符串末尾的空白字符
+        while (left <= right && s.charAt(right) == ' ') {
+            --right;
+        }
+
+        // 将字符串间多余的空白字符去除
+        StringBuilder sb = new StringBuilder();
+        while (left <= right) {
+            char c = s.charAt(left);
+
+            if (c != ' ') {
+                sb.append(c);
+            } else if (sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(c);
+            }
+
+            ++left;
+        }
+        return sb;
+    }
+
+    private void reverse(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char tmp = sb.charAt(left);
+            sb.setCharAt(left++, sb.charAt(right));
+            sb.setCharAt(right--, tmp);
+        }
+    }
+
+    private void reverseEachWord(StringBuilder sb) {
+        int n = sb.length();
+        int start = 0, end = 0;
+
+        while (start < n) {
+            // 循环至单词的末尾
+            while (end < n && sb.charAt(end) != ' ') {
+                ++end;
+            }
+            // 翻转单词
+            reverse(sb, start, end - 1);
+            // 更新start，去找下一个单词
+            start = end + 1;
+            ++end;
+        }
+    }
+}
+
+/**
+ * 双端队列
+ */
+class Main58_1{
+    public String reverseWords(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left <= right && s.charAt(left) == ' '){
+            ++left;
+        }
+        while (left <= right && s.charAt(right) == ' '){
+            --right;
+        }
+        Deque<String> d = new ArrayDeque<>();
+        StringBuilder word = new StringBuilder();
+        while (left <= right) {
+            char c = s.charAt(left);
+            if ((word.length() != 0) && (c == ' ')) {
+                // 将单词 push 到队列的头部
+                d.offerFirst(word.toString());
+                word.setLength(0);
+            } else if (c != ' ') {
+                word.append(c);
+            }
+            ++left;
+        }
+        d.offerFirst(word.toString());
+        return String.join(" ", d);
+    }
 }
---
+++ src/JZOffer/Main60.java
@@ -0,0 +1,13 @@
+package JZOffer;
+
+/**
+ * 动态规划
+ * 设输入n个骰子的解（即概率列表）为f(n),其中[点数和]为x的概率f(n,x)
+ * 当添加骰子的点数为 1 时，前 n - 1 个骰子的点数和应为 x - 1 ，方可组成点数和 x ；同理，当此骰子为 2 时，前 n - 1 个骰子应为 x - 2 ；以此类推，直至此骰子点数为 6 。将这 6 种情况的概率相加，即可得到概率 f(n, x)。递推公式如下所示：
+ *
+ */
+public class Main60 {
+    public double[] dicesProbability(int n) {
+        return null;
+    }
+}
