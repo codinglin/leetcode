90c530ed289d47e666266841525b22daa5c9a791
linjiayu <531467600@qq.com>
2022-10-30 13:49:20 +0800

    新的一天~

--- src/JZOffer/Main61.java
+++ src/JZOffer/Main61.java
@@ -1,4 +1,53 @@
 package JZOffer;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main61 {
+    public boolean isStraight(int[] nums) {
+        int max = -1, min = 14, countZero = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0){
+                countZero ++;
+                continue;
+            }
+            if(nums[i] > max){
+                max = nums[i];
+            }
+            if(nums[i] < min){
+                min = nums[i];
+            }
+            set.add(nums[i]);
+        }
+        if(max - min + 1 <= nums.length && set.size() + countZero == nums.length){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean isStraight1(int[] nums) {
+        Set<Integer> repeat = new HashSet<>();
+        int max = 0, min = 14;
+        for(int num : nums) {
+            if(num == 0) continue; // 跳过大小王
+            max = Math.max(max, num); // 最大牌
+            min = Math.min(min, num); // 最小牌
+            if(repeat.contains(num)) return false; // 若有重复，提前返回 false
+            repeat.add(num); // 添加此牌至 Set
+        }
+        return max - min < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
+    }
+
+    public boolean isStraight2(int[] nums) {
+        int joker = 0;
+        Arrays.sort(nums); // 数组排序
+        for(int i = 0; i < 4; i++) {
+            if(nums[i] == 0) joker++; // 统计大小王数量
+            else if(nums[i] == nums[i + 1]) return false; // 若有重复，提前返回 false
+        }
+        return nums[4] - nums[joker] < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
+    }
 }
