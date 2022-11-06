0400999b71b0e5cffc0238b4f87727ee69c9de90
linjiayu <531467600@qq.com>
2022-11-06 19:51:49 +0800

    菜鸡，爆发力量

--- src/problems/LeetCode40.java
+++ src/problems/LeetCode40.java
@@ -12,25 +12,27 @@ public class LeetCode40 {
         }
         // 关键步骤
         Arrays.sort(candidates);
-        boolean[] used = new boolean[candidates.length];
-        backtrack(candidates, 0, candidates.length, used, target, path, res);
+        backtrack(candidates, 0, length, target, path, res);
         return res;
     }
 
-    private void backtrack(int[] candidates, int begin, int length, boolean[] used, int target, Deque<Integer> path, List<List<Integer>> res) {
-        if(target < 0){
-            return;
-        }
+    private void backtrack(int[] candidates, int begin, int length, int target, Deque<Integer> path, List<List<Integer>> res) {
         if(target == 0){
             res.add(new ArrayList<>(path));
             return;
         }
-        if(used[begin]){
-            used[begin] = false;
-            path.add(candidates[begin]);
-            backtrack(candidates, begin + 1, length, used, target - candidates[begin], path, res);
+        for (int i = begin; i < length; i++) {
+            // 大剪枝：减去candidate[i] 小于0，减去后面的 candidate[i+1]、candidate[i+2]肯定也小于0，因此用 break;
+            if(target - candidates[i] < 0){
+                break;
+            }
+            // 小剪枝：同一层相同数值的结点，从第2个开始，候选数更少，因此跳过，用continue
+            if(i > begin && candidates[i] == candidates[i-1]){
+                continue;
+            }
+            path.add(candidates[i]);
+            backtrack(candidates, i+1, length, target - candidates[i], path, res);
             path.removeLast();
-            used[begin] = true;
         }
     }
 }
