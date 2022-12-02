fad12c0c22bc37a933a57eda1f862362b9653af9
linjiayu <531467600@qq.com>
2022-12-02 13:04:16 +0800

    新的一天

--- src/NiukeTOP101/BM88.java
+++ src/NiukeTOP101/BM88.java
@@ -1,4 +1,20 @@
 package NiukeTOP101;
 
 public class BM88 {
+    public boolean judge (String str) {
+        int n = str.length();
+        int mid = n / 2, left, right;
+        left = mid - 1;
+        if(n % 2 == 1){
+            right = mid + 1;
+        }else{
+            right = mid;
+        }
+        while (left >= 0 && right < n){
+            if(str.charAt(left--) != str.charAt(right++)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
---
+++ src/NiukeTOP101/BM89.java
@@ -0,0 +1,16 @@
+package NiukeTOP101;
+
+import java.util.ArrayList;
+
+public class BM89 {
+    public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+        return null;
+    }
+}
+
+class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+}
