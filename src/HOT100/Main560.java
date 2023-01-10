27a473b75211b2fd2a3fda2167d7e2ed97d24191
LinJiayu <531467600@qq.com>
2023-01-10 13:35:25 +0800

    Update Main560.java

--- src/HOT100/Main560.java
+++ src/HOT100/Main560.java
@@ -1,6 +1,15 @@
 package HOT100;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main560 {
+    /**
+     * 暴力解法
+     * @param nums
+     * @param k
+     * @return
+     */
     public static int subarraySum(int[] nums, int k) {
         int length = nums.length;
         int count = 0;
@@ -12,6 +21,59 @@ public class Main560 {
                     count ++;
                 }
             }
+        }
+        return count;
+    }
+
+    /**
+     * 前缀和
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum1(int[] nums, int k) {
+        int length = nums.length;
+        // 计算前缀和数组
+        int[] preSum = new int[length + 1];
+        preSum[0] = 0;
+        for (int i = 0; i < length; i++) {
+            preSum[i + 1] = preSum[i] + nums[i];
+        }
+        int count = 0;
+        for (int left = 0; left < length; left++) {
+            for (int right = left; right < length; right++) {
+                // 区间和 [left...right]，注意下标便宜
+                if(preSum[right + 1] - preSum[left] == k) {
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 前缀和 + 哈希表优化
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum2(int[] nums, int k) {
+        // key: 前缀和，value: key 对应的前缀和的个数
+        Map<Integer, Integer> preSumFreq = new HashMap<>();
+        // 对于下标为 0 的元素，前缀和为 0，个数为 1.
+        preSumFreq.put(0, 1);
+
+        int preSum = 0;
+        int count = 0;
+        for (int num : nums) {
+            preSum += num;
+            // 先获得前缀和为 preSum - k 的个数，加到计数变量里
+            // preSum - (preSum - k) == k 的区间的个数是我们所关心的
+            if(preSumFreq.containsKey(preSum - k)) {
+                count += preSumFreq.get(preSum - k);
+            }
+            // 然后维护 preSumFreq 的定义
+            preSumFreq.put(preSum, preSumFreq.getOrDefault(preSum, 0) + 1);
         }
         return count;
     }
