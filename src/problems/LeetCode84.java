e23359b2c1d21e994e941d30817a31c68774d801
LinJiayu <531467600@qq.com>
2023-01-11 23:07:49 +0800

    Update LeetCode84.java

--- src/problems/LeetCode84.java
+++ src/problems/LeetCode84.java
@@ -103,4 +103,37 @@ public class LeetCode84 {
         }
         return res;
     }
+
+    /**
+     * 单调栈 + 哨兵
+     * @param heights
+     * @return
+     */
+    public int largestRectangleArea3(int[] heights) {
+        int len = heights.length;
+        if(len == 0) {
+            return 0;
+        }
+        if(len == 1){
+            return heights[0];
+        }
+        int area = 0;
+        int[] newHeights = new int[len + 2];
+        for (int i = 0; i < len; i++) {
+            newHeights[i + 1] = heights[i];
+        }
+        len += 2;
+        heights = newHeights;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.addLast(0);
+        for (int i = 1; i < len; i++) {
+            while (heights[stack.peekLast()] > heights[i]) {
+                int height = heights[stack.removeLast()];
+                int width = i - stack.peekLast() - 1;
+                area = Math.max(area, width * height);
+            }
+            stack.addLast(i);
+        }
+        return area;
+    }
 }
