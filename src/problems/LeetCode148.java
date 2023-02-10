68a63ef6f3809fc4ebc2c76033505c8863af5489
LinJiayu <531467600@qq.com>
2023-02-10 21:45:58 +0800

    加油

---
+++ src/problems/LeetCode148.java
@@ -0,0 +1,37 @@
+package problems;
+
+public class LeetCode148 {
+    public ListNode sortList(ListNode head) {
+        if(head==null||head.next==null)return head;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode head2=slow.next;
+        slow.next=null;
+        return merge(sortList(head),sortList(head2));
+    }
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+}
---
+++ src/summarize/sort.md
@@ -0,0 +1,5 @@
+基础知识：快速排序（Quick Sort）， 归并排序（Merge Sort）的原理与代码实现。需要能讲明白代码中每一行的目的。快速排序时间复杂度平均状态下O（NlogN），空间复杂度O（1），归并排序最坏情况下时间复杂度O（NlogN），空间复杂度O（N）
+
+例题整理：
+
+[148. 排序链表](https://github.com/codinglin/leetcode/blob/master/src/problems/LeetCode148.java) (入门) 
