46f78dd969341584a04bb3f6890fa17d4640d872
LinJiayu <531467600@qq.com>
2023-02-13 17:08:48 +0800

    Update LeetCode264.java

--- src/problems/LeetCode264.java
+++ src/problems/LeetCode264.java
@@ -1,7 +1,28 @@
 package problems;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 public class LeetCode264 {
     public int nthUglyNumber(int n) {
-
+        int[] factors = {2, 3, 5};
+        Set<Long> set = new HashSet<>();
+        PriorityQueue<Long> heap= new PriorityQueue<>();
+        set.add(1L);
+        heap.add(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int factor : factors) {
+                long next = curr * factor;
+                if (set.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
     }
 }
