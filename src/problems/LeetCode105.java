6e1a1cbf9b1ad855a529df8eb1b6f8311c194d48
LinJiayu <531467600@qq.com>
2023-04-07 12:05:23 +0800

    ┭┮﹏┭┮

--- src/problems/LeetCode105.java
+++ src/problems/LeetCode105.java
@@ -76,3 +76,32 @@ class LeetCode105_1 {
         return root;
     }
 }
+
+class LeetCode105_2 {
+    private Map<Integer, Integer> indexMap;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return MyBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+    private TreeNode MyBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if(preorder_left > preorder_right) {
+            return null;
+        }
+        // 前序遍历第一个节点就是根节点
+        int preorder_root = preorder_left;
+        int inorder_root = indexMap.get(preorder[preorder_root]);
+        TreeNode root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        root.left = MyBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        root.right = MyBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
+    }
+}
