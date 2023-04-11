e63f2a6bf7c5c54e45fa37b4a30aa812ad92f35b
LinJiayu <531467600@qq.com>
2023-04-11 13:06:36 +0800

    Update LeetCode300.java

--- src/problems/LeetCode300.java
+++ src/problems/LeetCode300.java
@@ -27,3 +27,25 @@ public class LeetCode300 {
         return maxAns;
     }
 }
+
+/**
+ * 最长上升子序列
+ */
+class LeetCode300_1 {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+}
