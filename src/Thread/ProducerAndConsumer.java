ecf6cc652af2f3073cca73abe27dc1a91126d0b0
LinJiayu <531467600@qq.com>
2023-04-18 21:19:11 +0800

    心累

---
+++ src/Thread/ProducerAndConsumer.java
@@ -0,0 +1,78 @@
+package Thread;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ProducerAndConsumer {
+    private final int MAX_LEN = 10;
+    private final Queue<Integer> queue = new LinkedList<>();
+
+    class Producer extends Thread {
+        @Override
+        public void run() {
+            producer();
+        }
+
+        private void producer() {
+            while (true) {
+                synchronized (queue) {
+                    while (queue.size() == MAX_LEN) {
+                        queue.notify();
+                        System.out.println("当前队列满");
+                        try{
+                            queue.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    queue.add(1);
+                    queue.notify();
+                    System.out.println("生产者生产一条任务，当前队列长度为" + queue.size());
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    class Consumer extends Thread {
+        @Override
+        public void run() {
+            consumer();
+        }
+
+        private void consumer() {
+            while (true) {
+                synchronized (queue) {
+                    while (queue.size() == 0) {
+                        queue.notify();
+                        System.out.println("当前队列为空");
+                        try {
+                            queue.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    queue.poll();
+                    queue.notify();
+                    System.out.println("消费者消费一条任务，当前队列长度为" + queue.size());
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        ProducerAndConsumer pc = new ProducerAndConsumer();
+        Producer producer = pc.new Producer();
+        Consumer consumer = pc.new Consumer();
+        producer.start();
+        consumer.start();
+    }
+}
