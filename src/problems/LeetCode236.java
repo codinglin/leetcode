7201dd6670d57e3d8e766e1021f3eedd628e2f1c
LinJiayu <531467600@qq.com>
2023-04-21 11:48:30 +0800

    update

--- src/problems/LeetCode236.java
+++ src/problems/LeetCode236.java
@@ -24,3 +24,23 @@ public class LeetCode236 {
         return root;
     }
 }
+
+class LeetCode236_1 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null && right == null) {
+            return null;
+        }
+        if(left == null) {
+            return right;
+        }
+        if(right == null) {
+            return left;
+        }
+        return root;
+    }
+}
