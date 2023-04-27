5c0267344e599978a566178cff627952ea95815a
LinJiayu <531467600@qq.com>
2023-04-27 17:14:35 +0800

    Update LeetCode746.java

--- src/problems/LeetCode746.java
+++ src/problems/LeetCode746.java
@@ -2,6 +2,11 @@ package problems;
 
 public class LeetCode746 {
     public int minCostClimbingStairs(int[] cost) {
-        
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[n];
     }
 }
