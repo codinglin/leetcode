5bf907f7f4b3ab975d7c7e557077235573da895e
LinJiayu <531467600@qq.com>
2023-04-29 16:38:23 +0800

    0 offer的一天

--- src/problems/LeetCode1049.java
+++ src/problems/LeetCode1049.java
@@ -1,4 +1,27 @@
 package problems;
 
 public class LeetCode1049 {
+    public int lastStoneWeightII(int[] stones) {
+        int sum = 0;
+        for (int weight : stones) {
+            sum += weight;
+        }
+        int n = stones.length, m = sum / 2;
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if(j < stones[i]) {
+                    dp[i + 1][j] = dp[i][j];
+                } else {
+                    dp[i + 1][j] = dp[i][j] || dp[i][j - stones[i]];
+                }
+            }
+        }
+        for (int j = m;; --j) {
+            if (dp[n][j]) {
+                return sum - 2 * j;
+            }
+        }
+    }
 }
--- src/problems/LeetCode416.java
+++ src/problems/LeetCode416.java
@@ -1,5 +1,38 @@
 package problems;
 
 public class LeetCode416 {
-
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if(n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (int num : nums) {
+            sum += num;
+            maxNum = Math.max(maxNum, num);
+        }
+        if(sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        if(maxNum > target) {
+            return false;
+        }
+        boolean[][] dp = new boolean[n][target + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = 1; j <= target; j++) {
+                if(j >= num) {
+                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n - 1][target];
+    }
 }
