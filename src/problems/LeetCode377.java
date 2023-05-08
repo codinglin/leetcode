e12068fb3781a3bf1f414a25e34e0a50861067f9
LinJiayu <531467600@qq.com>
2023-05-08 11:48:14 +0800

    隔了好久

--- src/problems/LeetCode377.java
+++ src/problems/LeetCode377.java
@@ -1,4 +1,7 @@
 package problems;
 
 public class LeetCode377 {
+    public int combinationSum4(int[] nums, int target) {
+        return 0;
+    }
 }
--- src/problems/LeetCode474.java
+++ src/problems/LeetCode474.java
@@ -1,4 +1,30 @@
 package problems;
 
 public class LeetCode474 {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int length = strs.length;
+        int[][][] dp = new int[length + 1][m + 1][n + 1];
+        for (int i = 1; i <= length; i++) {
+            int[] zerosOnes = getZerosOnes(strs[i - 1]);
+            int zeros = zerosOnes[0], ones = zerosOnes[1];
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if(j >= zeros && k >= ones) {
+                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - zeros][k - ones] + 1);
+                    }
+                }
+            }
+        }
+        return dp[length][m][n];
+    }
+
+    private int[] getZerosOnes(String str) {
+        int[] zerosOnes = new int[2];
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            zerosOnes[str.charAt(i) - '0'] ++;
+        }
+        return zerosOnes;
+    }
 }
