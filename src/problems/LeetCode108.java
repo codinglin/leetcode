2099e7164ec784b5210329bf8c4ef6147f404fc1
LinJiayu <531467600@qq.com>
2023-05-11 19:23:46 +0800

    Update LeetCode108.java

--- src/problems/LeetCode108.java
+++ src/problems/LeetCode108.java
@@ -17,3 +17,21 @@ public class LeetCode108 {
         return root;
     }
 }
+
+class LeetCode108_1 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int left, int right) {
+        int mid = (left + right) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        if(left <= mid - 1) {
+            node.left = sortedArrayToBST(nums, left, mid - 1);
+        }
+        if(right >= mid + 1) {
+            node.right = sortedArrayToBST(nums, mid + 1, right);
+        }
+        return node;
+    }
+}
